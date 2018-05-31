42cf0e27724abacedd0270b1789c5748a4900645
capitainesushi <charlescooke@hotmail.com>
2018-05-31 18:53:13 -0400

    wtf

--- JeuEchec/JeuEchec/main.cpp
+++ JeuEchec/JeuEchec/main.cpp
@@ -6,8 +6,8 @@ and may not be redistributed without written permission.*/
 #include <stdio.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 1000;
 
 //Starts up SDL and creates window
 bool init();
